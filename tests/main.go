483b05b15d247377d77093a2a442fcdd5a6ee0c5
snowmerak <snowmerak@outlook.com>
2025-08-31 19:20:43 +0900

    refactor(tower): remove list operations and streamline map usage

--- tests/main.go
+++ tests/main.go
@@ -17,27 +17,9 @@ func main() {
 	}
 	defer db.Close()
 
-	db.CreateList("list")
-	db.PushLeft("list", tower.PrimitiveBool(true))
-	db.PushLeft("list", tower.PrimitiveInt(42))
-	db.PushLeft("list", tower.PrimitiveString("hello"))
-	db.PushLeft("list", tower.PrimitiveFloat(3.14))
-
-	for i := 0; i < 4; i++ {
-		val, _ := db.PopRight("list")
-		switch val.Type() {
-		case tower.TypeBool:
-			b, _ := val.Bool()
-			println("Bool:", b)
-		case tower.TypeInt:
-			n, _ := val.Int()
-			println("Int:", n)
-		case tower.TypeString:
-			s, _ := val.String()
-			println("String:", s)
-		case tower.TypeFloat:
-			f, _ := val.Float()
-			println("Float:", f)
-		}
-	}
+	db.CreateMap("example_map")
+	db.MapSet("example_map", tower.PrimitiveString("key1"), tower.PrimitiveString("value1"))
+	value, _ := db.MapGet("example_map", tower.PrimitiveString("key1"))
+	strValue, _ := value.String()
+	println("Retrieved value:", strValue)
 }
