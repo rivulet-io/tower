c0ea55a9469cf2eb8bfabc2fab1a0a88ea11099f
snowmerak <snowmerak@outlook.com>
2025-09-20 16:33:14 +0900

    feat(tower): enhance NewTower to support optional cluster, leaf, and client connections

--- tower.go
+++ tower.go
@@ -1,13 +1,21 @@
 package tower
 
-import "github.com/rivulet-io/tower/op"
+import (
+	"github.com/rivulet-io/tower/mesh"
+	"github.com/rivulet-io/tower/op"
+	"github.com/rivulet-io/tower/util/monad"
+)
 
 type Options struct {
-	Operator op.Options `json:"operator" yaml:"operator" toml:"operator"`
+	Operator op.Options                          `json:"operator" yaml:"operator" toml:"operator"`
+	Cluster  monad.Optional[mesh.ClusterOptions] `json:"cluster" yaml:"cluster" toml:"cluster"`
+	Leaf     monad.Optional[mesh.LeafOptions]    `json:"leaf" yaml:"leaf" toml:"leaf"`
+	Client   monad.Optional[mesh.ClientOptions]  `json:"client" yaml:"client" toml:"client"`
 }
 
 type Tower struct {
 	operator *op.Operator
+	cluster  mesh.WrapConn
 }
 
 func NewTower(opt *Options) (*Tower, error) {
@@ -16,9 +24,41 @@ func NewTower(opt *Options) (*Tower, error) {
 		return nil, err
 	}
 
-	return &Tower{
+	t := &Tower{
 		operator: operator,
-	}, nil
+	}
+
+	if opt.Cluster.IsSome() {
+		clusterOpt := opt.Cluster.Unwrap()
+		clusterConn, err := mesh.NewCluster(&clusterOpt)
+		if err != nil {
+			operator.Close()
+			return nil, err
+		}
+		t.cluster = clusterConn
+	}
+
+	if opt.Leaf.IsSome() {
+		leafOpt := opt.Leaf.Unwrap()
+		leafConn, err := mesh.NewLeaf(&leafOpt)
+		if err != nil {
+			operator.Close()
+			return nil, err
+		}
+		t.cluster = leafConn
+	}
+
+	if opt.Client.IsSome() {
+		clientOpt := opt.Client.Unwrap()
+		clientConn, err := mesh.NewClient(&clientOpt)
+		if err != nil {
+			operator.Close()
+			return nil, err
+		}
+		t.cluster = clientConn
+	}
+
+	return t, nil
 }
 
 func (t *Tower) Close() error {
